17444d836c83adb0ec20b58638955d85626c56a5
Sagar2366 <sagarutekar2366@gmail.com>
2022-10-14 10:23:26 +0530

    Adding go programs

--- golangNotes/programs/booking-app/main.go
+++ golangNotes/programs/booking-app/main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our conference booking application.")
+
+	var conferenceName = "Go Conference"
+	fmt.Println("Welcome to our", conferenceName, "conference booking application.")
 	fmt.Println("Get your tickets here to attent the conference.")
 }
